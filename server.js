dcb95482f57229536e278c4949af8511b4da3d3c
DragoonKite <frank.depaolo@yahoo.com>
2021-03-07 16:27:41 -0600

    moved server.js

--- server.js
+++ server.js
@@ -11,22 +11,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Data
-let notes = require('./db/db')
+let notes = require('./Develop/db/db')
 
 //Functions
-const saveNotes = function(){fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notes), (err) => 
+const saveNotes = function(){fs.writeFile(path.join(__dirname, '/Develop/db/db.json'), JSON.stringify(notes), (err) => 
     {
         if (err) throw err;
         console.log('The file has been saved!');
     });
 };
+
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'));
 });
 
 app.get('/api/notes', (req,res) => {
